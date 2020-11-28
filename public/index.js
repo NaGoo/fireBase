c66a7b89019ebe20242bf83f362a23dfb39b4016
NaGoo <nagucsengg@gmail.com>
2020-11-28 17:28:56 +0530

    firebaseconn

--- public/index.js
+++ public/index.js
@@ -1,51 +1,46 @@
+
+function onSubmit(){
+    docName=document.getElementById("dName").value;
+   var currentTime = new Date().toLocaleTimeString();
+   var currentDate = new Date().toLocaleDateString();
+   
+   firebase.firestore().collection('Data').doc(docName).set({
+       Name:document.getElementById("name").value ,
+       LastSync : currentDate +"____" +currentTime,
+   })
+document.getElementById("details").reset();
+location.reload();
+
+}
 document.addEventListener("DOMContentLoaded", event =>{      
-      const app = firebase.app();
-     y=document.getElementById("name").value
-     x=document.getElementById("dName").value;
-    var currentTime = new Date().toLocaleTimeString();
-    var currentDate = new Date().toLocaleDateString();
-    const db = firebase.firestore();
-    const docRef = db.collection('Data').doc('x');  
-    
-    docRef.set({
-        Name: y,
-        LastSync : currentDate +"____" +currentTime,
-    })
-const docR = db.collection('Data');
-docR.get()
+   
+    const app = firebase.app();
+
+firebase.firestore().collection('Data').get()
  .then(users=>{
-    
      users.forEach(doc=>{
          data = doc.data()
         console.log(data)
-let frag = document.createDocumentFragment();
-let tr = document.createElement('tr');
-let td1 = document.createElement('td');
-let td2 = document.createElement('td');
-td1.appendChild(document.createTextNode(data.Name));
-td2.appendChild(document.createTextNode(data.LastSync));
+   
 
-tr.appendChild(td1);
-tr.appendChild(td2);
-frag.appendChild(tr);
-view.appendChild(frag);
+    let frag = document.createDocumentFragment();
+    let tr = document.createElement('tr');
 
+      let td1 = document.createElement('td');
+       let td2 = document.createElement('td');
 
-})
+        td1.appendChild(document.createTextNode(data.Name));
+        td2.appendChild(document.createTextNode(data.LastSync));
+
+            tr.appendChild(td1);
+            tr.appendChild(td2);
+            frag.appendChild(tr);
+            view.appendChild(frag);
+
  })
-});
- //end
 
-  
-  
-//   function googleLogin(){
-//       const provider = new firebase.auth.GoogleAuthProvider();
-//       firebase.auth().signInWithPopup(provider)
-//       .then(result => {
-//           const user = result.user;
-//           document.write(`Email : ${user.email}`);
-//           console.log("output",user)
-//       })
-//       .catch(console.log)
-//   }
-  
+ })
+ 
+
+});
+ 
