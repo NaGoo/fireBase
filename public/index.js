b9855080dbb2932588bd5d03f63dcfe9e2b2f204
NaGoo <nagucsengg@gmail.com>
2020-11-28 15:36:10 +0530

    firebaseconn

--- public/index.js
+++ public/index.js
@@ -1,33 +1,42 @@
 document.addEventListener("DOMContentLoaded", event =>{      
       const app = firebase.app();
-  
-    
-  });
-
-  
-
-
-    function onSubmit(){
-
+     y=document.getElementById("name").value
+     x=document.getElementById("dName").value;
     var currentTime = new Date().toLocaleTimeString();
     var currentDate = new Date().toLocaleDateString();
     const db = firebase.firestore();
-    const docRef = db.collection('Data').doc(document.getElementById("dName").value);  
+    const docRef = db.collection('Data').doc('x');  
     
     docRef.set({
-        Name: document.getElementById("name").value,
+        Name: y,
         LastSync : currentDate +"____" +currentTime,
-
-    });
+    })
 const docR = db.collection('Data');
 docR.get()
  .then(users=>{
+    
      users.forEach(doc=>{
          data = doc.data()
         console.log(data)
-    })
+let frag = document.createDocumentFragment();
+let tr = document.createElement('tr');
+let td1 = document.createElement('td');
+let td2 = document.createElement('td');
+td1.appendChild(document.createTextNode(data.Name));
+td2.appendChild(document.createTextNode(data.LastSync));
+
+tr.appendChild(td1);
+tr.appendChild(td2);
+frag.appendChild(tr);
+view.appendChild(frag);
+
+
+})
  })
-  }
+});
+ //end
+
+  
   
 //   function googleLogin(){
 //       const provider = new firebase.auth.GoogleAuthProvider();
