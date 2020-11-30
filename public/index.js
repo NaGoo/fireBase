af7b7b06ec6d9c2a184e2c432dffab4759506f1a
NaGoo <nagucsengg@gmail.com>
2020-11-30 16:40:32 +0530

    updated

--- public/index.js
+++ public/index.js
@@ -21,25 +21,33 @@ firebase.firestore().collection('Data').get()
      users.forEach(doc=>{
          data = doc.data()
         console.log(data)
-   
-
     let frag = document.createDocumentFragment();
     let tr = document.createElement('tr');
-
       let td1 = document.createElement('td');
        let td2 = document.createElement('td');
        let td3 = document.createElement('td');
+       let td4 = document.createElement('button');
+       td4.innerHTML = '<i type="button" class="btn btn-danger" >Delete</i>';
 
         td1.appendChild(document.createTextNode(data.Name));
         td2.appendChild(document.createTextNode(data.LastSync));
         td3.appendChild(document.createTextNode(doc.id));
-
             tr.appendChild(td1);
             tr.appendChild(td2);
             tr.appendChild(td3);
+            tr.appendChild(td4);
             frag.appendChild(tr);
             view.appendChild(frag);
 
+            td4.addEventListener ("click", function() {
+
+                firebase.firestore().collection("Data").doc(doc.id).delete().then(function() {
+                    alert("Document successfully deleted!");
+                }).catch(function(error) {
+                    console.error("Error removing document: ", error);
+                });
+
+              });
  })
 
  })
