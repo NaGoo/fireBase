ba1bc3d801d89d5658dda926cedf29f111962759
NaGoo <nagucsengg@gmail.com>
2020-12-04 14:51:39 +0530

    updated

--- public/index.js
+++ public/index.js
@@ -11,29 +11,34 @@ function fnExcelReport()
     }
 
     tab_text=tab_text+"</table>";
-        sa = window.open('data:application/vnd.ms-excel,' + encodeURIComponent(tab_text));  
-
+        sa = window.open('data:application/vnd.ms-excel,' + encodeURIComponent(tab_text)); 
 }
-// function ExportToExcel(view){
-//     var htmltable= document.getElementById('view');
-//     var html = htmltable.outerHTML;
-//     window.open('data:application/vnd.ms-excel,' + encodeURIComponent(html));
-//  }
+  
 function onSubmit(){
     x=document.getElementById("dName").value;
    var currentTime = new Date().toLocaleTimeString();
-   var currentDate = new Date().toLocaleDateString();
-   
+   //var currentDate = new Date().toLocaleDateString();
+   //document.getElementById("timeIn").value=currentTime;
    firebase.firestore().collection('Data').doc(x).set({
        Name:document.getElementById("name").value ,
-       LastSync : currentDate +"____" +currentTime,
+       LastSync :currentTime,
+       TimeIn :document.getElementById("timeIn").value ,
+
+       TimeOut:document.getElementById("timeOut").value ,
+
    })
 document.getElementById("details").reset();
+//location.reload();
 
-}
+
+}      
+
 document.addEventListener("DOMContentLoaded", event =>{      
    
     const app = firebase.app();
+    document.getElementById("timeIn").disabled = true;
+    document.getElementById("timeOut").disabled = true;
+
 
  
 firebase.firestore().collection('Data').get()
@@ -43,36 +48,61 @@ firebase.firestore().collection('Data').get()
          console.log(data);
 
       let frag = document.createDocumentFragment(); let tr = document.createElement('tr');
-      let td1 = document.createElement('td'); let td2 = document.createElement('td'); let td3 = document.createElement('td'); 
-      let btn1 = document.createElement('td');  btn1.innerHTML = '<i type="button" class="btn btn-danger" >Delete</i>'; 
-      let btn2 = document.createElement('td');  btn2.innerHTML = '<i type="button" class="btn btn-primary" >Edit</i>';
+      let user = document.createElement('td'); let documentname = document.createElement('td'); let login = document.createElement('td'); logout = document.createElement('td'); 
+      let del = document.createElement('td');  del.innerHTML = '<i type="button" class="btn btn-danger" >Delete</i>'; 
+      let edits = document.createElement('td');  edits.innerHTML = '<i type="button" class="btn btn-primary" >update</i>';
+      let log = document.createElement('td');  log.innerHTML = '<i type="button" class="btn btn-success" >LogOut</i>';
 
-       td1.appendChild(document.createTextNode(data.Name)); td2.appendChild(document.createTextNode(data.LastSync)); td3.appendChild(document.createTextNode(doc.id));
+       user.appendChild(document.createTextNode(data.Name)); login.appendChild(document.createTextNode(data.TimeIn));
+        documentname.appendChild(document.createTextNode(doc.id)); logout.appendChild(document.createTextNode(data.TimeOut)); 
+ 
 
-            tr.appendChild(td1); tr.appendChild(td2); tr.appendChild(td3);  tr.appendChild(btn1);
-            tr.appendChild(btn2); frag.appendChild(tr); view.appendChild(frag);
 
+            tr.appendChild(user);tr.appendChild(documentname); tr.appendChild(login); tr.appendChild(log); tr.appendChild(logout); 
+            tr.appendChild(edits);  tr.appendChild(del); frag.appendChild(tr); view.appendChild(frag);
 
-             btn2.addEventListener("click",function(){
-                 var rename = "";
+            log.addEventListener("click",function (){
+                var tim = new Date().toLocaleTimeString();
+                //var dat = new Date().toLocaleDateString();
+                document.getElementById("timeOut").value=tim ;    //+"----"+dat;
+                var rename =data.Name;
                  var docs = doc.id;
+                 var newtime= data.LastSync;
+                 //console.log(newtime)
                  document.getElementById("name").value = rename;
                  document.getElementById("dName").value = docs;
+                 document.getElementById("timeIn").value = newtime;
+                // onSubmit();
+            })
+
+             edits.addEventListener("click",function(){
+                 alert("you should change only")
+                 var rename = "";
+                 var docs = doc.id;
+                 var newtimein= data.TimeIn;
+                 var newtimeout= data.TimeOut;
+                 document.getElementById("name").value = rename;
+                 document.getElementById("dName").value = docs;
+                 document.getElementById("timeIn").value = newtimein;
+                 document.getElementById("timeOut").value = newtimeout;
+
+              // onSubmit();
             });
            
-            btn1.addEventListener ("click", function() {
+            del.addEventListener ("click", function() {
 
                 firebase.firestore().collection("Data").doc(doc.id).delete().then(function() {
                     alert("Document successfully deleted!");
+                    
                 }).catch(function(error) {
                     console.error("Error removing document: ", error);
                 });
 
             });
+            
  })
 
  })
  
-
 });
  
